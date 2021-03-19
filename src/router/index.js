2059acd892872ca9b6f9124f1b3fc707d0bc8715
GulajavaMinistudio <gulajava.mini@gmail.com>
2021-03-19 22:56:02 +0700

    Update package starter

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,32 @@
 import { createRouter, createWebHashHistory } from 'vue-router';
-import Home from '../views/Home.vue';
+
+// route level code-splitting
+// this generates a separate chunk (about.[hash].js) for this route
+// which is lazy-loaded when the route is visited.
+const HomepageComponent = () => import(/* webpackChunkName: "about" */ '../views/Homepage.vue');
+const HomeComponent = () => import(/* webpackChunkName: "about" */ '../views/Home.vue');
+const AboutComponent = () => import(/* webpackChunkName: "about" */ '../views/About.vue');
 
 const routes = [
     {
         path: '/',
+        name: 'Homepage',
+        component: HomepageComponent,
+    },
+    {
+        path: '/home',
         name: 'Home',
-        component: Home,
+        component: HomeComponent,
     },
     {
         path: '/about',
         name: 'About',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+        component: AboutComponent,
+    },
+    {
+        path: '/:catchAll(.*)',
+        name: 'NotFound',
+        redirect: { name: 'Homepage' },
     },
 ];
 
---
+++ src/views/Homepage.vue
@@ -0,0 +1,14 @@
+<template>
+    <div></div>
+</template>
+
+<script>
+export default {
+    setup() {
+        // fungsi composition api berjalan sebelum lifecycle hooks dimulai
+        return {};
+    },
+};
+</script>
+
+<style lang="scss"></style>
